02645c5580c373954564dbd46cb34dae4726f574
VC-Cr3ate <vincenthe634@gmail.com>
2025-07-04 13:00:29 +0800

    new pyworld

--- helloworld.py
+++ helloworld.py
@@ -9,7 +9,7 @@ def main():
     while True:
         command = input("Command: ").strip().lower()
         if command == "inc":
-            counter += 2
+            counter += 6
             print(f"Counter is now: {counter}")
         elif command == "exit":
             print("Goodbye!")
